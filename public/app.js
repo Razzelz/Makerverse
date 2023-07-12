889205917fc7992db1b844d052cdca3d70da63e8
maddie <d00397898@dmail.dixie.edu>
2023-07-12 09:38:45 -0600

    Frontend work

--- public/app.js
+++ public/app.js
@@ -5,7 +5,14 @@ Vue.createApp({
         }
     },
     methods : {
+            getMakes: function() {
+                    fetch("http://localhost:8080/blueprints/")
+                            .then(response => response.json()).then((data) => {
+                                    this.makes = data;
+                            })
+            }
     },
     created : function() {
+	    this.getMakes();
     }
 }).mount("#app");
