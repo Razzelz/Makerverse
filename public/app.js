f709ad81f5a4984596239a81fca04e493465189d
maddie <d00397898@dmail.dixie.edu>
2023-07-12 13:17:23 -0600

    front-end work

--- public/app.js
+++ public/app.js
@@ -10,7 +10,19 @@ Vue.createApp({
                             .then(response => response.json()).then((data) => {
                                     this.makes = data;
                             })
-            }
+            },
+	    viewMake: function() {
+		    console.log("This takes to new page");
+	    },
+	    likeMake: function() {
+		    console.log("This likes a make");
+	    },
+	    downloadMake: function() {
+		    console.log("This downloads a make");
+	    },
+	    home: function() {
+		    console.log("This takes to home page");
+	    }
     },
     created : function() {
 	    this.getMakes();
